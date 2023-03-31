9bff19c83ac99873e7ae699ce040807b184a516b
teeny16 <dandiaz10@yahoo.com>
2023-03-31 15:58:59 +0800

    nextauth add scope

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+
+
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
@@ -51,8 +53,8 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      authorization:
-        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
+      authorization: "https://accounts.spotify.com/authorize",
+      scope: "user-read-email playlist-read-private user-read-email streaming user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state user-read-recently-played user-follow-read"
     }),
   ],
 
