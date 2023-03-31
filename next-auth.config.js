7301b4e46431999179cc94fe0a463d1c0574e20d
Dan Anfernee Diaz <109204793+ferneediaz@users.noreply.github.com>
2023-03-31 19:47:13 +0800

    Delete next-auth.config.js

--- next-auth.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-      // Add your providers here
-    ],
-    callbacks: {
-      // Add your callbacks here
-    },
-  };
