38397c48577256582f9202d5e252668f2f888625
teeny16 <dandiaz10@yahoo.com>
2023-04-02 00:34:37 +0800

    updated nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,18 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
 async function refreshAccessToken(token) {
   try {
     const url =
       "https://accounts.spotify.com/api/token?" +
       new URLSearchParams({
-          client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-          client_secret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+        client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+        client_secret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
         grant_type: "refresh_token",
         refresh_token: token.refreshToken,
       });
@@ -45,8 +49,8 @@ async function refreshAccessToken(token) {
 export default NextAuth({
   providers: [
     SpotifyProvider({
-      clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-      clientSecret: NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+      client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+        client_secret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
       authorization:
         "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
     }),
@@ -84,6 +88,3 @@ export default NextAuth({
 
 
 
-
-// client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-// client_secret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
