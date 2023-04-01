cfa0ed594a0e40ea4d6c5c9daed8c04d07665e21
teeny16 <dandiaz10@yahoo.com>
2023-04-01 23:25:59 +0800

    fixed bug sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -10,7 +10,7 @@ import {
   import { RiCompassFill } from "react-icons/ri";
   import Image from "next/image";
   
-  const Sidebar () => {
+  const Sidebar = () => {
     return (
       <div className="fixed top-0 z-40 flex flex-col p-4 items-center bg-black w-[90px] h-screen space-y-8">
         <Image
